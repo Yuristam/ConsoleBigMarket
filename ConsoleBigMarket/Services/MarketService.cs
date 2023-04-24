02602c80619b3f629af531a5ed6e537f3437b885
Yuristam <yuristamasanakunov@gmail.com>
2023-04-24 22:36:50 +0600

    fixed some little bugs

--- ConsoleBigMarket/Services/MarketService.cs
+++ ConsoleBigMarket/Services/MarketService.cs
@@ -9,10 +9,19 @@
             while (true)
             {
                 Console.Clear();
+                Console.SetCursorPosition(0,0);
                 Console.Write("This is Market's Main Room\n\n" +
                     "Enter your product(s) to buy\n" +
                     "If you completed your shopping, enter 'exit'\n\n>");
+                
+                Console.SetCursorPosition(0,8);
+                Console.Write("Your Products List:\n"+ new string('-', 40));
+                for (int i = 0; i < basket.Count; i++)
+                {
+                    Console.Write($"\n{i + 1}) {basket[i].Name} {basket[i].Price}$");
+                }
 
+                Console.SetCursorPosition(1, 5);
                 Product product = new Product();
                 product.Name = Console.ReadLine().ToLower().Trim();
                 if (product.Name == "exit")
@@ -41,7 +50,7 @@
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Your Products List:\n");
+                Console.WriteLine("Your Products List:\n"+ new string('-', 40));
 
                 float total = 0;
                 for (int i = 0; i < basket.Count; i++)
@@ -50,7 +59,7 @@
                     total += basket[i].Price;
                 }
 
-                Console.WriteLine(new string('-',25) + $"\nTotal: {total} $");
+                Console.WriteLine(new string('-',40) + $"\nTotal: {total} $");
 
                 Console.Write("\n\nEnter OK to buy all products\n\n" +
                     "If you want to delete product, enter the name of the product then enter remove\n\n");
@@ -59,26 +68,27 @@
 
                 for (int i = 0; i < basket.Count; i++)
                 {
-                    if (userInput == $"{i} delete")
-                        basket.RemoveAt(i - 1);
+                    if (userInput == $"delete {i+1}")
+                        basket.RemoveAt(i);
                 }
 
                 if (userInput == "ok")
                 {
+                    Console.Clear();
                     Console.WriteLine("Thank you for the purchase!");
                     Console.WriteLine("Press any key to go to the main menu.");
                     Console.ReadKey();
                     Console.Clear();
                     break;
                 }
-                else
+               /* else
                 {
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.Write("You've typed wrong symbol, please type letters.");
                     Thread.Sleep(1000);
                     Console.ResetColor();
-                }
+                }*/
             }
         }
     }
